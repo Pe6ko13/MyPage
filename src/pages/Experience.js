7049331b32827849db4afaecc293ba8c38ca3ce3
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-06-13 16:31:12 +0300

    add info to experience page

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -14,47 +14,48 @@ function Experience() {
             <VerticalTimeline lineColor='#434443'>
                 <VerticalTimelineElement
                     className='vertical-timeline-element--education'
-                    date=''
+                    date='2002-2007'
                     iconStyle={{ background: '#434443', color: '#fff' }}
                     icon={<SchoolIcon />}
                 >
                     <h3 className='vertical-timeline-element-title'>
-                        University of Varna
+                        High school of Natural Sciences and Mathematics
                     </h3>
-                    <p>Tele engineer</p>
+                    <p>Matematics and geography</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className='vertical-timeline-element--education'
-                    date=''
+                    date='2007-2012'
                     iconStyle={{ background: '#434443', color: '#fff' }}
                     icon={<SchoolIcon />}
                 >
                     <h3 className='vertical-timeline-element-title'>
-                        University of Varna
+                        Technical University of Varna
                     </h3>
-                    <p>Tele engineer</p>
+                    <p>Telecommunication engineering</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className='vertical-timeline-element--education'
-                    date=''
+                    date='2013-2020'
                     iconStyle={{ background: '#55ee90', color: '#fff' }}
                     icon={<JobIcon />}
                 >
                     <h3 className='vertical-timeline-element-title'>
-                        University of Varna
+                        DHL Aiation ltd
                     </h3>
-                    <p>Tele engineer</p>
+                    <h4>East Midlands Airport</h4>
+                    <p>Ramp Agent</p>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className='vertical-timeline-element--education'
-                    date=''
+                    date='2020 - present'
                     iconStyle={{ background: '#434443', color: '#fff' }}
                     icon={<SchoolIcon />}
                 >
                     <h3 className='vertical-timeline-element-title'>
-                        University of Varna
+                        Software University Sofia
                     </h3>
-                    <p>Tele engineer</p>
+                    <p>Full-stack Web developer</p>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
