913052b05060f8ced30d3c5ff7131785dd2561e8
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-07-07 21:43:54 +0300

    change home page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
             <BrowserRouter>
                 <Navbar />
                 <Routes>
-                    <Route path='/' element={<Home />} />
+                    <Route path='/MyPage' element={<Home />} />
                     <Route path='/projects' element={<Projects />} />
                     <Route path='/project/:id' element={<ProjectDisplay />} />
                     <Route path='/experience' element={<Experience />} />
