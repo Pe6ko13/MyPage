214004d559d2f024a9ac67524fae0f32bd718c13
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-07-07 17:52:07 +0300

    add some styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,22 +7,35 @@ import '../styles/Home.css';
 function Home() {
     return (
         <div className='home'>
-            <div className='about'>
-                <h2>Hello,</h2>
-                <h1>I am Petar Belchev</h1>
-                <div className='prompt'>
-                    <p>A software developer...</p>
-                    <a href='https://www.linkedin.com/in/petar-belchev/'>
-                        <LinkedInIcon />
-                    </a>
-                    <a href='https://github.com/Pe6ko13'>
-                        <GithubIcon />
-                    </a>
-                    <a href='https://mail.google.com/mail/u'>
-                        <EmailIcon />
-                    </a>
+            <div className='aboutWrap'>
+                <div className='about'>
+                    <h2>Hello,</h2>
+                    <h1>I am Petar Belchev</h1>
+                    <div className='prompt'>
+                        <p>
+                            Software developer who focuses on writing clean and
+                            efficient code.
+                        </p>
+                        <a href='https://www.linkedin.com/in/petar-belchev/'>
+                            <LinkedInIcon />
+                        </a>
+                        <a href='https://github.com/Pe6ko13'>
+                            <GithubIcon />
+                        </a>
+                        <a href='https://mail.google.com/mail/u'>
+                            <EmailIcon />
+                        </a>
+                    </div>
+                </div>
+                <div className='aboutPhoto'>
+                    <img
+                        className='myPhoto'
+                        src='https://avatars.githubusercontent.com/u/77295463?v=4'
+                        alt='image'
+                    />
                 </div>
             </div>
+
             <div className='skills'>
                 <h2> Skills</h2>
                 <ol className='list'>
