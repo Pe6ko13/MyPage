44c29a32e9c822b3e1303d2a8ae399f01110a3c1
Petar Belchev <pe6ko13@yahoo.co.uk>
2022-08-11 16:38:28 +0300

    add projects to the list

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,9 @@
 import QuizImage from '../assets/quiz.jpg';
 import SearchImage from '../assets/search-address.png';
 import cookBook from '../assets/cookBook.png';
+import car_img from '../assets/car.png';
+import recipe_img from '../assets/recipe_img.jpg';
+import pets_img from '../assets/pets_img';
 
 export const projectList = [
     {
@@ -20,5 +23,24 @@ export const projectList = [
         image: cookBook,
         technologies: 'HTML, CSS, JavaScript',
         link: 'https://github.com/CookBook-js-apps/CookBook-js-apps.github.io',
+    },
+    {
+        name: 'Car-app Server',
+        image: car_img,
+        technologies:
+            'JavaScript, Node.js, Express, MongoDB, Mongoose, Handlebars',
+        link: 'https://github.com/Pe6ko13/JavaScript--BackEnd/tree/main/car-site-workshop',
+    },
+    {
+        name: 'Famous Recipes',
+        image: recipe_img,
+        technologies: 'React, HTML, Styled Components',
+        link: 'https://github.com/Pe6ko13/React_projects/tree/main/recipe_app',
+    },
+    {
+        name: 'My Pets',
+        image: pets_img,
+        technologies: 'React, HTML, CSS, Firebase Authentication',
+        link: 'https://github.com/Pe6ko13/React_projects/tree/main/my-pets/application',
     },
 ];
